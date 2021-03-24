67f2049c2d1a0fba16c5356cae6f9e6ba88688a5
anviettrung <an.viettrung.m@gmail.com>
2021-03-24 18:14:47 +0700

    update W5: sort & remove result

--- Week5/sort.cpp
+++ Week5/sort.cpp
@@ -15,27 +15,30 @@ bool AscendingOrder(int e_left, int e_right);
 // Sorting algorithm
 // ================================
 typedef bool (*SortOrder)(int e1, int e2);
-typedef int (*SortFunction)(int** a, int n, SortOrder ordered);
+typedef int (*SortFunction)(int* a, int n, SortOrder ordered);
 
 // --------------------------------
-int InsertionSort(int** a, int n, SortOrder ordered);
-int SelectionSort(int** a, int n, SortOrder ordered);
-int BubbleSort(int** a, int n, SortOrder ordered);
+int InsertionSort(int* a, int n, SortOrder ordered);
+int SelectionSort(int* a, int n, SortOrder ordered);
+int BubbleSort(int* a, int n, SortOrder ordered);
+// // --------------------------------
+int MergeSort(int* a, int n, SortOrder ordered);
+void MergeSortFullCall(int* a, int left, int right, SortOrder ordered);
+void Merge(int* a, int left, int mid, int right, SortOrder ordered);
+// // --------------------------------
+int HeapSort(int* a, int n, SortOrder ordered);
+void Heapify(int* a, int n, int nodeID, SortOrder ordered);
 // --------------------------------
-int MergeSort(int** a, int n, SortOrder ordered);
-void MergeSortFullCall(int** a, int left, int right, SortOrder ordered);
-void Merge(int** a, int left, int mid, int right, SortOrder ordered);
-// --------------------------------
-// int HeapSort();
-// int QuickSort();
-// int ShellSort();
-// int CountingSort();
+// int QuickSort(int* a, int n, SortOrder ordered);
+// int ShellSort(int* a, int n, SortOrder ordered);
+// int CountingSort(int* a, int n, SortOrder ordered);
 
 SortFunction f_sorts[] = {
 	InsertionSort,
 	SelectionSort,
 	BubbleSort,
-	MergeSort
+	MergeSort,
+	HeapSort
 };
 
 int main()
@@ -60,7 +63,7 @@ int main()
 	// PROCESS
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-	f_sorts[fsortIndex](&arr, n, AscendingOrder);
+	f_sorts[fsortIndex](arr, n, AscendingOrder);
 
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
 	milliseconds ms_int = duration_cast<milliseconds>(t2 - t1);
@@ -94,62 +97,62 @@ bool AscendingOrder(int e_left, int e_right)
 // ================================
 // Sorting algorithm
 // ================================
-int InsertionSort(int** a, int n, SortOrder ordered)
+int InsertionSort(int* a, int n, SortOrder ordered)
 {
 	int key, j;
 	for (int i = 1; i < n; i++) {
 
-		key = (*a)[i];
+		key = a[i];
 		j = i - 1;
 
-		while (j >= 0 && !ordered((*a)[j], key)) {
-			(*a)[j + 1] = (*a)[j];
+		while (j >= 0 && !ordered(a[j], key)) {
+			a[j + 1] = a[j];
 			j -= 1;
 		}
 
-		(*a)[j + 1] = key;
+		a[j + 1] = key;
 	}
 
 	return 0;
 }
 
-int SelectionSort(int** a, int n, SortOrder ordered)
+int SelectionSort(int* a, int n, SortOrder ordered)
 {
 	int min;
 	int e;
 	for (int i = 0; i < n; i++) {
-		min = (*a)[i];
+		min = a[i];
 		e = i;
 		for (int j = i+1; j < n; j++)
-			if (!ordered(min, (*a)[j])) {
-				min = (*a)[j];
+			if (!ordered(min, a[j])) {
+				min = a[j];
 				e = j;
 			}
-		Swap(&(*a)[i], &(*a)[e]);
+		Swap(&a[i], &a[e]);
 	}
 
 	return 0;
 }
 
-int BubbleSort(int** a, int n, SortOrder ordered)
+int BubbleSort(int* a, int n, SortOrder ordered)
 {
 	for (int i = 0; i < n; i++)
 		for (int j = i+1; j < n; j++)
-			if (!ordered((*a)[i], (*a)[j]))
-				Swap(&(*a)[i], &(*a)[j]);
+			if (!ordered(a[i], a[j]))
+				Swap(&a[i], &a[j]);
 
 	return 0;
 }
 
 // --------------------------------
-int MergeSort(int** a, int n, SortOrder ordered)
+int MergeSort(int* a, int n, SortOrder ordered)
 {
 	MergeSortFullCall(a, 0, n-1, ordered);
 
 	return 0;
 }
 
-void MergeSortFullCall(int** a, int left, int right, SortOrder ordered)
+void MergeSortFullCall(int* a, int left, int right, SortOrder ordered)
 {
 	if (left < right) {
 		int mid = left + (right - left) / 2;
@@ -160,7 +163,7 @@ void MergeSortFullCall(int** a, int left, int right, SortOrder ordered)
 	}
 }
 
-void Merge(int** a, int left, int mid, int right, SortOrder ordered)
+void Merge(int* a, int left, int mid, int right, SortOrder ordered)
 {
 	int n1 = mid - left + 1;
 	int n2 = right - mid;
@@ -169,10 +172,10 @@ void Merge(int** a, int left, int mid, int right, SortOrder ordered)
 	int Larr[n1], Rarr[n2];
 
 	for (int i = 0; i < n1; i++)
-		Larr[i] = (*a)[left + i];
+		Larr[i] = a[left + i];
 
 	for (int i = 0; i < n2; i++)
-		Rarr[i] = (*a)[mid + i + 1];
+		Rarr[i] = a[mid + i + 1];
 
 	int track1 = 0;
 	int track2 = 0;
@@ -180,30 +183,69 @@ void Merge(int** a, int left, int mid, int right, SortOrder ordered)
 
 	while (track1 < n1 && track2 < n2) {
 		if (ordered(Larr[track1], Rarr[track2])) {
-			(*a)[k] = Larr[track1];
+			a[k] = Larr[track1];
 			track1++;
 		} else {
-			(*a)[k] = Rarr[track2];
+			a[k] = Rarr[track2];
 			track2++;
 		}
 		k++;
 	}
 
 	while (track1 < n1) {
-		(*a)[k] = Larr[track1];
+		a[k] = Larr[track1];
 		track1++;
 		k++;
 	}
 
 	while (track2 < n2) {
-		(*a)[k] = Rarr[track2];
+		a[k] = Rarr[track2];
 		track2++;
 		k++;
 	}
 }
 // --------------------------------
+int HeapSort(int* a, int n, SortOrder ordered)
+{
+	// Build heap
+	for (int i = n/2 - 1; i >= 0; i--)
+		Heapify(a, n, i, ordered);
 
-// int HeapSort();
-// int QuickSort();
-// int ShellSort();
-// int CountingSort();
+	// Extract elements form heap
+	for (int i = n-1; i > 0; i--) {
+		Swap(&a[0], &a[i]);
+
+		// reduce heap
+		Heapify(a, i, 0, ordered);
+	}
+
+	return 0;
+}
+
+void Heapify(int* a, int n, int nodeID, SortOrder ordered)
+{
+	int largest = nodeID;
+	int left = 2 * nodeID + 1;
+	int right = 2 * nodeID + 2;
+
+	// Left child larger than root
+	if (left < n)
+		if (!ordered(a[left], a[largest]))
+			largest = left;
+
+	// Right child larger than root
+	if (right < n)
+		if (!ordered(a[right], a[largest]))
+			largest = right;
+
+	if (largest != nodeID) {
+		Swap(&a[nodeID], &a[largest]);
+
+		Heapify(a, n, largest, ordered);
+	}
+}
+// --------------------------------
+
+// int QuickSort(int** a, int n, SortOrder ordered);
+// int ShellSort(int** a, int n, SortOrder ordered);
+// int CountingSort(int** a, int n, SortOrder ordered);
